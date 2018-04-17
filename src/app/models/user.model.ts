b0fcaabb0969b0e9910138a52622aea7b37fb44f
Vishwas <vishwasgv93@gmail.com>
2018-04-17 08:54:26 +0530

    Adding username fields

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -5,14 +5,16 @@ export class User{
     public userName: string;
     public phoneNumber: number;
     public dateOfBirth: Date;
+    public password: string;
 
     constructor(firstName: string, lastName: string, email: string, userName: string,
-        phoneNumber: number, dateOfBirth: Date){
+        phoneNumber: number, dateOfBirth: Date, password: string){
             this.firstName = firstName;
             this.lastName = lastName;
             this.email = email;
             this.userName = userName;
             this.dateOfBirth = dateOfBirth;
+            this.password = password;
     }
 
 }

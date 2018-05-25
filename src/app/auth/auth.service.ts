ffb423f40377b73660c958c09d22290319a16919
Vishwas <M1028002@mindtree.com>
2018-05-25 17:06:31 +0530

    Adding login endpoint

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import * as firebase from "firebase";
 import { Router } from "@angular/router";
+import { Http, Headers, RequestOptions, Response } from "@angular/http";
 
 @Injectable()
 export class AuthService{
@@ -8,13 +9,13 @@ export class AuthService{
     // token : string;
     validUser : boolean = false;
 
-    constructor(private router: Router) {}
+    constructor(private router: Router, private http: Http) {}
 
     isUserAuthenticated () {
         return this.validUser;
     }
 
-    signInValidate ( userName : string, password : string ){
+    signInValidate ( email : string, password : string ){
         // firebase.auth().signInWithEmailAndPassword(userName, password)
         //     .then(response => {
         //         this.router.navigate(['/dashboard']);
@@ -25,12 +26,20 @@ export class AuthService{
         //         )
         //     });
 
-        const email = "test@test.com";
-        const pwd = "Test@123";
+        // const email1 = "test@test.com";
+        // const pwd = "Test@123";
 
-        if(userName === email && password === pwd){
-           return this.validUser = true; 
-        }
+        // if(email1 === email && password === pwd){
+        //    return this.validUser = true; 
+        // }
+        let headers = new Headers({'Content-type' : 'application/json'});
+        let options = new RequestOptions({headers : headers});
+        
+        this.http.post('http://localhost:8008/login',
+            JSON.stringify({emailAddress: email, password: password}), options).
+            map((response: Response)=> {
+                console.log(response.json);
+            }).subscribe();
 
         return this.validUser;
     }
